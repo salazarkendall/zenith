36df157a6e936efc59c6daa183d9aa29b8d808ab
devken <ken-dev@outlook.com>
2026-05-09 16:19:55 -0600

    [main] Initialize Spring Boot project with Maven wrapper, base application, and configuration files

---
+++ src/main/java/com/kendev/zenith/ZenithApplication.java
@@ -0,0 +1,13 @@
+package com.kendev.zenith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenithApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ZenithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kendev/zenith/ZenithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kendev.zenith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
